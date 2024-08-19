c368faec218660408aec7de1e9ceeec6dcb09625
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2024-08-19 12:38:02 +0100

    One last try at stupid case insensive thing

--- compile.py
+++ compile.py
@@ -199,9 +199,8 @@ def process_lesson(path,action_counts,plumed_syntax,eggdb=None):
              #subprocess.run(config["command"])
 
         # Check for the existence of a NAVIGATION file
-        if not os.path.exists("data/NAVIGATION.md") : 
-           if os.path.exists("data/navigation.md") : os.rename("data/navigation.md","data/NAVIGATION.md")
-           else : raise RuntimeError("No NAVIGATION.md file found in lesson")
+        if os.path.exists("data/navigation.md") : os.rename("data/navigation.md","data/NAVIGATION.md")
+        if not os.path.exists("data/NAVIGATION.md") : raise RuntimeError("No NAVIGATION.md file found in lesson")
         # Get the contents of the embeds file
         embeds = {}
         if os.path.exists("data/embed.yml") :
